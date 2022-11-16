39ed9466ce21040b27c21fc374b3f46a9f23579b
980035530 <980035530@LAPTOP-TK7DCT0U>
2022-11-16 14:37:01 -0700

    Prueba num 1.01

---
+++ src/git_17/Git_17.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_17;
+
+/**
+ *
+ * @author 980035530
+ */
+public class Git_17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("B I E N V E NI D OS ");
+    }
+    
+}
